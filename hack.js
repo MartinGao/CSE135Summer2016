d597cbd10375e520dd6d719655a61a8f3e0af195
Martin Gao <Marting.Gao@Gmail.com>
2016-09-04 20:57:38 -0700

    Update hack.js

--- hack.js
+++ hack.js
@@ -1,5 +1,5 @@
-if(confirm('Sorry bro but your site is hacked :( You have to pay my $1M dollar or your site would be mine.')){
-  alert('Deal!');
+if(confirm('Sorry bro but your site is hacked :( \nYou have to pay me $1M dollar or your site would be mine.')){
+  alert('Deal! Enjoy CSE 135 :)');
 } else{
   alert('Ok. Say goodbye to your site.');window.location='http://goo.gl/zKYKat';
 }
